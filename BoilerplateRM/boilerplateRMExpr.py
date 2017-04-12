cf18caeac375c1746d3ea655464378038d722656
Alexander Nwala <anwala@cs.odu.edu>
2017-04-12 15:40:16 -0400

    code to replicate survey of boilerplate rm methods

--- BoilerplateRM/boilerplateRMExpr.py
+++ BoilerplateRM/boilerplateRMExpr.py
@@ -53,17 +53,18 @@ def testBoilerplateRM(folderName):
 
 def entryPoint():
 
-	sampleURIs =[
-			"http://www.wsj.com/articles/indiana-gives-7-million-in-tax-breaks-to-keep-carrier-jobs-1480608461",
-			"http://www.cnbc.com/2016/12/01/howard-schultz-stepping-down-as-starbucks-ceo.html",
-			"http://www.usatoday.com/story/news/nation-now/2016/12/01/death-toll-tennessee-wildfires-rises-10/94756844/?utm_source=dlvr.it&utm_medium=twitter",
-			"https://www.washingtonpost.com/politics/shouting-match-erupts-between-clinton-and-trump-aides/2016/12/01/7ac4398e-b7ea-11e6-b8df-600bd9d38a02_story.html?utm_term=.f17f35b548b4",
-			"http://www.telegraph.co.uk/football/2016/12/01/arsenal-should-glad-efl-cup/",
-			"http://nymag.com/thecut/2016/12/victorias-secret-angel-jasmine-tookes-diet-workout-routine.html",
-			"http://www.nytimes.com/2016/12/19/world/europe/russia-ambassador-shot-ankara-turkey-report.html",
-			"http://www.latimes.com/local/lanow/la-me-san-bernardino-terror-probe-20161130-story.html",
-			"http://www.wsj.com/articles/wells-fargo-to-keep-commissions-based-retirement-accounts-under-fiduciary-rule-1480615211",
-			"http://www.theatlantic.com/entertainment/archive/2016/12/trevor-noah-finds-his-late-night-voice/509318/"]
+	sampleURIs = [
+					"http://www.wsj.com/articles/indiana-gives-7-million-in-tax-breaks-to-keep-carrier-jobs-1480608461",
+					"http://www.cnbc.com/2016/12/01/howard-schultz-stepping-down-as-starbucks-ceo.html",
+					"http://www.usatoday.com/story/news/nation-now/2016/12/01/death-toll-tennessee-wildfires-rises-10/94756844/?utm_source=dlvr.it&utm_medium=twitter",
+					"https://www.washingtonpost.com/politics/shouting-match-erupts-between-clinton-and-trump-aides/2016/12/01/7ac4398e-b7ea-11e6-b8df-600bd9d38a02_story.html?utm_term=.f17f35b548b4",
+					"http://www.telegraph.co.uk/football/2016/12/01/arsenal-should-glad-efl-cup/",
+					"http://nymag.com/thecut/2016/12/victorias-secret-angel-jasmine-tookes-diet-workout-routine.html",
+					"http://www.nytimes.com/2016/12/19/world/europe/russia-ambassador-shot-ankara-turkey-report.html",
+					"http://www.latimes.com/local/lanow/la-me-san-bernardino-terror-probe-20161130-story.html",
+					"http://www.wsj.com/articles/wells-fargo-to-keep-commissions-based-retirement-accounts-under-fiduciary-rule-1480615211",
+					"http://www.theatlantic.com/entertainment/archive/2016/12/trevor-noah-finds-his-late-night-voice/509318/"
+				]
 
 	shortURIs = [ 'https://archive.is/VWV9j',
 				  'https://archive.is/NiUZp',
